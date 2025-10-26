5dc86e53536c3d86cc30b69481d142b5ec904dd6
pardis-21 <pardisehsani@cmail.carleton.ca>
2025-10-26 12:43:46 -0400

    commiting games

---
+++ Game.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+import java.util.*;
+
+public class Game {
+    Scanner numPlayer = new Scanner(System.in);
+
+    System.out.println("Welcome to UNO");
+    System.out.println("Number of players?: " + %numPlayers);
+
+
+}
--- GameLogic.java
+++ GameLogic.java
@@ -4,9 +4,22 @@ public class GameLogic {
     private ArrayList<Player> players;
     private ArrayList<Card> cards;
     private ArrayList<Card> hand;
+    private ArrayList<Card> discardPile;
+    private ArrayList<Card> drawPile;
+    private ArrayList<Card> flipPile;
+    private boolean direction; //clockwise or counterclockwise
+    private int score;
+
 
     public GameLogic() {
-
+        players = new ArrayList<>();
+        cards = new ArrayList<>();
+        hand = new ArrayList<>();
+        discardPile = new ArrayList<>();
+        flipPile = new ArrayList<>();
+        drawPile = new ArrayList<>();
+        direction = true; //clockwise direction
+        score = 0;
     }
 
 
