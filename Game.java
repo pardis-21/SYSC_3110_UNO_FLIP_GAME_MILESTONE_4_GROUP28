8860c41bb458b9298b8ebe975f073b162a98964d
Anvita Ala <anvita632.a@gmail.com>
2025-11-10 21:29:42 -0500

    cleaned it out!

--- Game.java
+++ Game.java
@@ -9,75 +9,8 @@ import java.util.*;
  * @Author Pardis Ehsani 101300400
  */
 public class Game {
-    static public ArrayList<Player> players = new ArrayList<>();
-    public int numPlayers;
-    PlayerOrder playerOrder = new PlayerOrder();
 
     public Game() {
-        Scanner userInput = new Scanner(System.in);
-        numPlayers = 0;
-
-        System.out.println("Welcome to UNO");
-        System.out.println("---------------------------------------");
-
-//       ____________________CODE MOVED TO UNOFRAME________________
-//        // get number of players
-//        while (true) {
-//            System.out.print("Enter the number of Players (2–4): ");
-//            try {
-//                numPlayers = userInput.nextInt();
-//                userInput.nextLine();
-//                if (numPlayers < 2 || numPlayers > 4) {
-//                    System.out.println("Invalid number of players! Please enter 2–4.\n");
-//                } else break;
-//            } catch (InputMismatchException e) {
-//                System.out.println("Invalid input! Please enter a number between 2 and 4.\n");
-//                userInput.nextLine();
-//            }
-//        }
-//
-//        // get player names
-//        while (players.size() < numPlayers) {
-//            System.out.print("Enter player name: ");
-//            String playerName = userInput.nextLine().trim();
-//
-//            //make sure 2 players by same name don't exist
-//            boolean exists = false;
-//            for (Player player : players) {
-//                if (player.getName().equals(playerName)) {
-//                    System.out.println("That player already exists.\n");
-//                    exists = true;
-//                    break;
-//                }
-//            }
-//            if (!exists) {
-//                addNewPlayer(playerName);
-//            }
-//        }
-
-
-        //I HAD TO COMMENT OUT FOR GUI TO SHOW
-
-//        // initialize GameLogic ONCE, so scores persist
-//        GameLogic gameLogic = new GameLogic(players);
-//        gameLogic.setPlayerOrder(playerOrder);
-//        final int TARGET = 500;
-//
-//        // play multiple rounds until someone wins the match
-//        while (true) {
-//            System.out.println("\n===== STARTING NEW ROUND =====\n");
-//            gameLogic.playUNOGame();
-//
-//            Player matchWinner = gameLogic.getMatchWinner(TARGET);
-//            if (matchWinner != null) {
-//                System.out.println(matchWinner.getName() + " reached " + TARGET + " points!");
-//                System.out.println("GAME OVER!");
-//                break;
-//            }
-//
-//            System.out.println("No one has reached " + TARGET + " points yet. Press ENTER to start a new round.");
-//            userInput.nextLine();
-//        }
 
     }
 
